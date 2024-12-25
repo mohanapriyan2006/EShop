cf9068b16bdf7e025d5333faef802fbdaa936ab3
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2024-12-25 12:31:59 +0530

    version 2

--- collection.js
+++ collection.js
@@ -3,17 +3,23 @@ var search = document.getElementById("search");
 var productList = productContainer.querySelectorAll("div")
 
 search.addEventListener("keyup",function(event){
-     var enteredValue = event.target.value.toLowerCase();
-
-     for(count=0;count<productList.length;count=count+1){
+    var enteredValue = event.target.value.toLowerCase();
+    
+    for(count=0;count<productList.length;count=count+1){
         var productName = productList[count].querySelector("p").textContent;
-
+        
         if(productName.toLowerCase().indexOf(enteredValue)<0){
             productList[count].style.display ="none";
         }
         else{
             productList[count].style.display ="block";
         }
-     }
+    }
 })
 
+
+
+var phNo = document.getElementById("phno");
+function subscribe(phNo){
+    alert("Subscription done by this ->"+phNo)
+}
--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function showNav(){
 function closeNav(){
     sidenav.style.left="-50%";
 }
+
+var phNo = document.querySelector("#phno");
+function subscribe(){
+    var phNoV = phNo.value;
+    var lOfNo = phNoV.length;
+    var no = parseInt(phNoV);
+    if(no > 0 & lOfNo ==10){
+        alert("Subscription done by this ->"+no)
+    }
+    else{
+        alert("Plz enter valid Phone number !");
+    }
+    console.log(phNo);
+}
+
+
+function placeOrder(){
+    alert("Successfully Place the Order (~.~) \n        Have a nice day :)")
+}
